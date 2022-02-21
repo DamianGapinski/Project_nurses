4a51affc544cfae52e2b8a64f09207603ef56ce1
DamianGapinski <71337183+DamianGapinski@users.noreply.github.com>
2022-02-21 11:24:57 -0800

    Add files via upload
    fixed addeventlisener

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 window.sr = ScrollReveal();
 sr.reveal('.img_ga, .img_happy, .img_price, p, h2, i, ul, .img, h3',{delay:300})
 
-window.onload = ()=>{
-    let btn = document.querySelector("#btn");
-    btn.addEventListener("click", topFunction);
-} 
 
 
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+const burger_btn = document.querySelector('.burger_btn');
 
-
-let burgerBtn = document.querySelector('.burger_btn');
 const nav = document.querySelector('nav');
 const burgerBarA = document.querySelector('.burger_barA');
 const burgerBarB = document.querySelector('.burger_barB');
 const burgerBarC = document.querySelector('.burger_barC');
 
-burgerBtn = document.addEventListener('click', () =>{
-  nav.classList.toggle('nav_on')
-  burgerBarA.classList.toggle('burger_barA_On')
-  burgerBarB.classList.toggle('burger_barB_On')
-  burgerBarC.classList.toggle('burger_barC_On')
+burger_btn.addEventListener('click',  () =>{
+  nav.classList.toggle('nav_on');
+  burgerBarA.classList.toggle('burger_barA_On');
+  burgerBarB.classList.toggle('burger_barB_On');
+  burgerBarC.classList.toggle('burger_barC_On');
 
 })
