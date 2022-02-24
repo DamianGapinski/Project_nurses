16d8f8dfe658988fda4b5244fc3953775956772a
DamianGapinski <71337183+DamianGapinski@users.noreply.github.com>
2022-02-23 17:21:44 -0800

    Add files via upload
    end page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.sr = ScrollReveal();
-sr.reveal('.img_ga, .img_happy, .img_price, p, h2, i, ul, .img, h3',{delay:300})
+sr.reveal('.img_ga, .img_happy, .img_price, p, h2, i,ul, .img, h3',{delay:300})
 
 
 
@@ -16,4 +16,47 @@ burger_btn.addEventListener('click',  () =>{
   burgerBarB.classList.toggle('burger_barB_On');
   burgerBarC.classList.toggle('burger_barC_On');
 
+});
+
+const what_we_do = document.querySelector('.what_we_do')
+const for_patients = document.querySelector('.for_patients')
+const prise = document.querySelector('.prise')
+const cadre = document.querySelector('.cadre')
+const contact = document.querySelector('.contact')
+
+what_we_do.addEventListener('click',()=>{
+  window.scrollTo({
+    top:320,
+    left:0,
+    behavior:"smooth"
+  })
+})
+
+for_patients.addEventListener('click',()=>{
+  window.scrollTo({
+    top:1350,
+    left:0,
+    behavior:"smooth"
+  })
+})
+prise.addEventListener('click',()=>{
+  window.scrollTo({
+    top:2500,
+    left:0,
+    behavior:"smooth"
+  })
+})
+cadre.addEventListener('click',()=>{
+  window.scrollTo({
+    top:3550,
+    left:0,
+    behavior:"smooth"
+  })
+})
+contact.addEventListener('click',()=>{
+  window.scrollTo({
+    top:4800,
+    left:0,
+    behavior:"smooth"
+  })
 })
